4057af1e26376591cee9b9e25048d764c1527b04
ruiyu <zhangruiyu@hexindai.com>
2018-11-09 17:57:52 +0800

    卡包

--- lib/core/cardbag/card_bag_manager_screen.dart
+++ lib/core/cardbag/card_bag_manager_screen.dart
@@ -22,6 +22,7 @@ class _CardBagManagerScreenState extends State<CardBagManagerScreen> {
   @override
   Widget build(BuildContext context) {
     var edgeInsets = new EdgeInsets.only(top: 10.0);
+    var theme = Theme.of(context);
     const textStyle = const TextStyle();
     return new StoreBuilder<AppState>(builder: (context, store) {
       var currentUser = store.state.currentUser;
@@ -48,7 +49,76 @@ class _CardBagManagerScreenState extends State<CardBagManagerScreen> {
                       .toList(),
                 ),
               ),
-              body: new TabBarView(children: <Widget>[Text("1"), Text("2")])));
+              body: new TabBarView(children: <Widget>[
+                ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      key: Key(index.toString()),
+                      color: Colors.purple,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.all(15.0),
+                            padding: const EdgeInsets.all(18.0),
+                            child: Text(
+                              '900元',
+                              style: theme.textTheme.subhead.merge(TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 19.0)),
+                            ),
+                            decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5.0))),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Text("抵扣剪发卡",
+                                        textAlign: TextAlign.left,
+                                        style: theme.textTheme.subhead.merge(
+                                          TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0,
+                                          ),
+                                        )),
+                                    Text("X10",
+                                        textAlign: TextAlign.right,
+                                        style: theme.textTheme.subhead.merge(
+                                          TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.0),
+                                        )),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: Text("可以抵扣剪发,一次性可使用多张"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {},
+                            child: Text("转为准备中"),
+                            height: 100.0,
+                            color: Colors.tealAccent,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: 20,
+                ),
+                Text("2")
+              ])));
     });
   }
 }
